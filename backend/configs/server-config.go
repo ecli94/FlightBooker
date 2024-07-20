55b29694f6843dfd3ca4b15174db0706a4e9e0d0
Echo Li <echo.li@inmeta.no>
2024-07-20 16:24:52 +0200

    Added web server skeleton

---
+++ backend/configs/server-config.go
@@ -0,0 +1,8 @@
+package configs
+
+const (
+	MONGODB_URI             string = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority"
+	SERVER_STATIC_DIRECTORY string = "./web/static"
+	SERVER_URL              string = "localhost"
+	SERVER_PORT             string = "8081"
+)
--- backend/src/flightbooker/go.mod
+++
@@ -1,3 +0,0 @@
-module FlightBooker/backend/src/flightbooker
-
-go 1.22.4
