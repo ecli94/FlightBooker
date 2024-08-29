1c253af73903789ef5565b9fd54a80944a0b4ce9
Arman <arman.noor3@gmail.com>
2024-08-29 15:06:35 +0200

    implemented location search using google api

--- flightbooker/src/components/Home/DestinationToCard.tsx
+++ flightbooker/src/components/Home/DestinationToCard.tsx
@@ -4,24 +4,35 @@ import { Dispatch, forwardRef, SetStateAction } from 'react';
 
 interface DestinationToCard {
     closeCard: () => void;
-    to: number | undefined;
-    from: number | undefined;
-    setTo: Dispatch<SetStateAction<number | undefined>>;
-    setFrom: Dispatch<SetStateAction<number | undefined>>;
+    to: string | undefined;
+    from: string | undefined;
+    setTo: Dispatch<SetStateAction<string | undefined>>;
+    setFrom: Dispatch<SetStateAction<string | undefined>>;
 }
 
 const DestinationToCard = forwardRef<HTMLDivElement, DestinationToCard>((props, ref) => {
+    const direction = 'Destinations';
     return (
         <div className={styles.overlay} onClick={props.closeCard}>
-            <div ref={ref} id="to" className={styles.card} onClick={(e) => e.stopPropagation()}>
+            <div ref={ref} id="to" className={styles.cityCard} onClick={(e) => e.stopPropagation()}>
+                <div className={styles.passengerHeadingContainer}>
+                    <span className={styles.cardHeading}> {direction} </span>
+                    <button className={styles.passengerHeadingCloseButton} onClick={props.closeCard}>
+                        {' '}
+                        X{' '}
+                    </button>
+                </div>
                 <SelectDestination
-                    direction="Destinations"
+                    direction={direction}
                     closeCallback={props.closeCard}
                     to={props.to}
                     setTo={props.setTo}
                     from={props.from}
                     setFrom={props.setFrom}
                 />
+                <div className={styles.passengerDoneButton} style={{ marginTop: '250px' }}>
+                    <button onClick={props.closeCard}> Done </button>
+                </div>
             </div>
         </div>
     );
--- flightbooker/src/components/Home/SelectedDestination.tsx
+++ flightbooker/src/components/Home/SelectedDestination.tsx
@@ -1,44 +1,77 @@
 import { Dispatch, SetStateAction } from 'react';
-import useContextHandler from '../../hooks/useHomeBookingCard';
+import { APILoader, PlacePicker } from '@googlemaps/extended-component-library/react';
 import styles from '../../styles/Home.module.css';
 
 interface SelectDestinationProps {
     direction: 'Departure locations' | 'Destinations';
     closeCallback: () => void;
-    to: number | undefined;
-    setTo: Dispatch<SetStateAction<number | undefined>>;
-    from: number | undefined;
-    setFrom: Dispatch<SetStateAction<number | undefined>>;
+    to: string | undefined;
+    setTo: Dispatch<SetStateAction<string | undefined>>;
+    from: string | undefined;
+    setFrom: Dispatch<SetStateAction<string | undefined>>;
 }
 
 const SelectDestination: React.FC<SelectDestinationProps> = (props) => {
-    const { locationsToDispatch, locationsFromDispatch, locationTo, locationFrom } = useContextHandler();
-
-    const handleLocationChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const id = parseInt(event.target.value, 10);
+    const cleanLocation = (loc: string) => {
+        const locArray = loc.split(',');
+        let cleanCity;
+        const uncleanCity = locArray[locArray.length - 2].trim();
+        switch (uncleanCity.split(' ').length) {
+            case 1:
+                cleanCity = uncleanCity;
+                break;
+            case 2:
+                cleanCity = uncleanCity.split(' ')[1];
+                break;
+            default:
+                cleanCity = uncleanCity;
+        }
+        console.log(uncleanCity);
+        console.log(cleanCity);
+        return (cleanCity ? cleanCity : uncleanCity) + ',' + locArray[locArray.length - 1];
+    };
+    const handleLocationChange = (event: any) => {
+        // const id = parseInt(event.target.value, 10);
         switch (props.direction) {
             case 'Departure locations':
-                locationsFromDispatch({ type: 'COMPLETE', id: id });
+                // locationsFromDispatch({ type: 'COMPLETE', id: id });
                 props.closeCallback();
-                props.setFrom(locationFrom.find((loc) => loc.id == id)?.id);
+                // props.setFrom(locationFrom.find((loc) => loc.id == id)?.id);
+                props.setFrom(cleanLocation(event.target.value?.formattedAddress) ?? '');
                 break;
             case 'Destinations':
-                locationsToDispatch({ type: 'COMPLETE', id: id });
+                // locationsToDispatch({ type: 'COMPLETE', id: id });
                 props.closeCallback();
-                props.setTo(locationTo.find((loc) => loc.id == id)?.id);
+                // props.setTo(locationTo.find((loc) => loc.id == id)?.id);
+                props.setTo(cleanLocation(event.target.value?.formattedAddress) ?? '');
                 break;
             default:
                 throw new TypeError('Incorrect direction was provided');
         }
     };
+    // current scope is only within Norway
+    const countries: string[] = ['no'];
 
     return (
         <>
             <div className={styles.citySelectVerticalMenu}>
-                <label className={styles.cardHeading}>
-                    <span> {props.direction}</span>
-                </label>
-                <select
+                <APILoader
+                    // api key will be ommitted when pushed to github.
+                    // must implement env variables and use that instead here
+                    apiKey={'YOUR-API-KEY'}
+                    solutionChannel={'GMP_GCC_placepicker_to'}
+                />
+                <div>
+                    <PlacePicker
+                        className={styles.airportPicker}
+                        country={countries}
+                        type="airport"
+                        itemType="city"
+                        placeholder="Enter a location"
+                        onPlaceChange={handleLocationChange}
+                    />
+                </div>
+                {/* <select
                     id="locations"
                     name="locations"
                     size={8}
@@ -57,7 +90,7 @@ const SelectDestination: React.FC<SelectDestinationProps> = (props) => {
                                 {t.city}, {t.country}
                             </option>
                         ))}
-                </select>
+                </select> */}
             </div>
         </>
     );
